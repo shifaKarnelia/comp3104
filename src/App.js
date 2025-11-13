e5adede205cd6a92e109771a9dea56838093ffe7
shifa <shifa.karnelia@georgebrown.ca>
2025-11-13 16:05:39 -0500

    name changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101487402 || shifa karnelia || DevOps</h5>
+      <h5>Developed By: 101487402 || shifa karnelia || DevOps||comp3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
